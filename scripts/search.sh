e967d224b58ef6d3f01f6d850f5d6b1fa70d4d08
Bruno Thomsen <bruno.thomsen@gmail.com>
2015-07-01 22:13:32 +0200

    search.sh: updated regex and print statistics
    Signed-off-by: Bruno Thomsen <bruno.thomsen@gmail.com>

--- scripts/search.sh
+++ scripts/search.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-set -u # exit if an uninitialised variable is used
+set -u # exit if an uninitialized variable is used
 set -e # exit if any statement returns a non-true value
 
 OUT_DIR="../out"
 ID_OUT="${OUT_DIR}/ids.txt"
 VID_OUT="${OUT_DIR}/vids.txt"
 PID_OUT="${OUT_DIR}/pids.txt"
+
 
 if [[ "$#" -ne 1 ]]; then
 	echo "Usage $0 SOURCE"
@@ -16,12 +17,17 @@ else
 	INPUT=$1
 fi
 
+
 if [[ -d "$INPUT" ]]; then
 	# Locate C #defines with at least a 2-byte hex value
-	grep "define" "${INPUT}/drivers/usb/" -R | grep -E "0x[a-zA-Z0-9]{4}" > ${ID_OUT}
+	grep "define" "${INPUT}" -R | grep -E "0x[a-fA-F0-9]{4}" > ${ID_OUT}
 	# Locate USB Vendor ID define candidates
 	grep "VID" < ${ID_OUT} > ${VID_OUT}
 	# Locate USB Product ID define candidates
 	grep "PID" < ${ID_OUT} > ${PID_OUT}
-fi	
+	# Print statistics
+	vid_cnt=$(wc -l < ${VID_OUT})
+	pid_cnt=$(wc -l < ${PID_OUT})
+	echo "Found ${vid_cnt} VIDs and ${pid_cnt} PIDs that should be evaluated."
+fi
 
