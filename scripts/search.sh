5139a4128cfc86f3bce167aea56b21e7f04426d2
Bruno Thomsen <bruno.thomsen@gmail.com>
2015-07-09 23:04:35 +0200

    scripts/search.sh: verified vids/pids support
    This is basic filtering of vids or pids that has been added to database.
    
    Example:
    Adding a pid define to verified/pid_defines.txt marks it as verified.
    
    Signed-off-by: Bruno Thomsen <bruno.thomsen@gmail.com>

--- scripts/search.sh
+++ scripts/search.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
-set -u # exit if an uninitialized variable is used
-set -e # exit if any statement returns a non-true value
+set -e # Exit script if any statement returns a non-true value.
+set -u # Exit script if using an uninitialized variable.
+
+################################################################################
+# DEFINES
+################################################################################
+
+INPUT=""
 
 OUT_DIR="../out"
 ID_OUT="${OUT_DIR}/ids.txt"
 VID_OUT="${OUT_DIR}/vids.txt"
 PID_OUT="${OUT_DIR}/pids.txt"
+VID_MISSING="${OUT_DIR}/vids_missing.txt"
+PID_MISSING="${OUT_DIR}/pids_missing.txt"
 
+VERIFIED_DIR="../verified"
+VID_VERIFIED="${VERIFIED_DIR}/vid_defines.txt"
+PID_VERIFIED="${VERIFIED_DIR}/pid_defines.txt"
+
+
+################################################################################
+# MAIN
+################################################################################
 
 if [[ "$#" -ne 1 ]]; then
 	echo "Usage $0 SOURCE"
@@ -19,6 +35,7 @@ fi
 
 
 if [[ -d "$INPUT" ]]; then
+	mkdir -p "${OUT_DIR}"
 	# Locate C #defines with at least a 2-byte hex value
 	grep "define" "${INPUT}" -R | grep -E "0x[a-fA-F0-9]{4}" > ${ID_OUT}
 	# Locate USB Vendor ID define candidates
@@ -30,4 +47,19 @@ if [[ -d "$INPUT" ]]; then
 	pid_cnt=$(wc -l < ${PID_OUT})
 	echo "Found ${vid_cnt} VIDs and ${pid_cnt} PIDs that should be evaluated."
 fi
+
+cp "${VID_OUT}" "${VID_MISSING}"
+while read define; do
+	if [[ -n "${define}" ]]; then
+		sed -i "/$define/d" "${VID_MISSING}"
+	fi
+done < "${VID_VERIFIED}"
+
+cp "${PID_OUT}" "${PID_MISSING}"
+while read define; do
+	if [[ -n "${define}" ]]; then
+		sed -i "/$define/d" "${PID_MISSING}"
+	fi
+done < "${PID_VERIFIED}"
+
 
